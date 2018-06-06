0d80c6903a090b419ec6760299bf4585634e01b1
makhija0211 <mahimamakhija0211@gmail.com>
2018-06-06 15:18:53 +0530

    Deleted folder webapp

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include , url
 
 urlpatterns = [
-    path('webapp/', include('webapp.urls')),
+  
     path('admin/', admin.site.urls),
 ]
